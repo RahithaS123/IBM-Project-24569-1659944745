4629ac47180e871ca2e7df090e11aa6836460fe9
sawdeshwaran <98637478+sawdeshwaran@users.noreply.github.com>
2022-11-04 11:30:12 +0530

    Update app.py

--- Assignment 2/app.py
+++ Assignment 2/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template
 
+app = Flask(_name_)
+
+@app.route("/")
+@app.route("/home")
+def home():
+  return render_template("home.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+@app.route("/signin")
+def signin():
+  return render_template("signin.html")
+
+@app.route("/signup")
+def signup():
+  return render_template("signup.html")
